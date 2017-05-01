374ff016f1105ea08d43f60996e9cde40614e61a
lmxyy <664789531@qq.com>
2017-05-02 01:24:43 +0800

    initial commit

---
+++ src/Train.hpp
@@ -0,0 +1,54 @@
+#ifndef SJTU_GENERALUSER_HPP
+#define SJTU_GENERALUSER_HPP
+
+#include<string>
+#include"Exceptions.hpp"
+#include"File.hpp"
+#include"lib/ptr.hpp"
+#include"GeneralUser"
+
+using std::string;
+using namespace sjtu
+{
+	class Train
+	{
+	private:
+		bool on_sale; vector <Station> route;
+		string train_id; Date departure_time;
+		//构造函数
+		Train(const bool &_on_sale,const vector <Station> _route,const string &_train_id,const Date &_date)
+			:on_sale(_on_sale),route(_route),train_id(_train_id),departure_time(_date){}
+		
+		//默认析构函数
+
+		//返回id
+		string get_id() const { return train_id; }
+
+		//返回发车时间
+		Date get_time() const { return departure_time; }
+
+		//返回出站(start_time)在start_time到finish_time之间的票，票数不够就返回所有的票,无票throw
+		Tickets get_ticket(const string &start_station,const string &finish_station,const Date &start_time,const Date &finish_time,const string &level,int number) const;
+
+		//所有站名
+		shared_ptr < vector <string> > query_station() const
+		{
+
+		}
+
+		//买票, 判断区间是否可减以及余额是否够，若true则减少money，且若true则改区间信息
+		bool buy_ticket(const string &start_station,const string &finish_station,const string &level,int number,double &money);
+
+		//退票, 增加money,改区间信息
+		bool refund_ticket(const string &start_station,const string &finish_station,const string &level,int number,double &money);
+
+
+		//开始发售
+		bool start_sale() {}
+
+		//结束发售
+		bool finish_sale() {}
+	}
+}
+
+#endif
