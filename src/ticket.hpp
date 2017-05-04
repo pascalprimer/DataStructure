05e28064b1826d155de6ca49544afce6bbe91efc
GrahamYan <yange98@sjtu.edu.cn>
2017-05-04 12:02:06 +0800

    ticket 1.0

--- src/ticket.hpp
+++ src/ticket.hpp
@@ -40,47 +40,46 @@ class ticket{
 			price = obj.price;
 		}
 		ticket &operator = (const ticket &obj) {
-			ticket tmp;
-			tmp.train_id = obj.train_id;
-			tmp.departure_time = obj.departure_time;
-			tmp.start_station = obj.start_station;
-			tmp.finish_station = obj.finish_station;
-			tmp.level = obj.level;
-			tmp.number = obj.number;
-			tmp.price = obj.price;
+			train_id = obj.train_id;
+			departure_time = obj.departure_time;
+			start_station = obj.start_station;
+			finish_station = obj.finish_station;
+			level = obj.level;
+			number = obj.number;
+			price = obj.price;
 			
-			return tmp;
+			return *this;
 		}
-		bool operator == (const ticket &obj) {
+/*		bool operator == (const ticket &obj) {
 			if (train_id == obj.train_id && departure_time == obj.departure_time && strat_station == obj.start_station && finish_station == obj.finish_station && level == obj.level) {
 				return true;
 			}
 			else return false;
 		}
 		bool operator != (const ticket &obj) {
-			if (train_id == obj.train_id && departure_time == obj.departure_time && strat_station == obj.start_station && finish_station == obj.finish_station && level == obj.level) {
+			if (train_id == obj.train_id && departure_time == obj.departure_time && start_station == obj.start_station && finish_station == obj.finish_station && level == obj.level) {
 				return false;
 			}
 			else return true;
-		}
-		bool operator < (const ticket &obj) {
-			if (*this -> train_id < obj.train_id) {
+		}*/
+		bool operator < (ticket &obj) {
+			if (train_id < obj.train_id) {
 				return true;
 			}
-			else if (*this -> train_id == obj.train_id) {
-				if (*this -> departure_time < obj.departure_time) {
+			else if (train_id == obj.train_id) {
+				if (departure_time < obj.departure_time) {
 					return true;
 				}
-				else if (*this -> departure_time == obj.departure_time) {
-					if (*this -> start_station < obj.start_station) {
+				else if (departure_time == obj.departure_time) {
+					if (start_station < obj.start_station) {
 						return true;
 					}
-					else if (*this -> start_station == obj.start_station) {
-						if (*this -> finish_station < obj.finish_station) {
+					else if (start_station == obj.start_station) {
+						if (finish_station < obj.finish_station) {
 							return true;
 						}
-						else if (*this -> finish_station == obj.finish_station) {
-							if (*this -> level < obj.level) {
+						else if (finish_station == obj.finish_station) {
+							if (level < obj.level) {
 								return true;
 							}
 							else {
@@ -92,7 +91,7 @@ class ticket{
 			}
 			return false;
 		}
-		const string query_number() {
+		const int query_number() {
 			return number;
 		} 
 		const date query_date() {
@@ -108,11 +107,13 @@ class ticket{
 			return finish_station;
 		}
 		bool modify_number (int delta) {
-			if (*this -> number + delta >= 0) {
-				*this -> number += delta;
+			if (number + delta >= 0) {
+				number += delta;
 				return true;
 			}
 			else return false;
 		}
 };
 }
+
+#endif 
